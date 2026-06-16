a35cc537dd4af860db07db7a0a480ec23676f984
isidorapanic <isidora9panic@gmail.com>
2026-06-16 12:35:49 +0200

    napravljena strukutra

---
+++ FrizerskiSalon/Repositories/TerminiRepo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrizerskiSalon.Repositories
+{
+    internal class TerminiRepo
+    {
+    }
+}
