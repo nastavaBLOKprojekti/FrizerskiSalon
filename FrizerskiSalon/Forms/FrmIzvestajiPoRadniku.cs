ddc4252b70820ccfb945433156865035351e057e
andjelamajstor <andjela2008.am@gmail.com>
2026-06-16 09:49:13 -0700

    uradjen izvestaj po radniku.

--- FrizerskiSalon/Forms/FrmIzvestajiPoRadniku.cs
+++ FrizerskiSalon/Forms/FrmIzvestajiPoRadniku.cs
@@ -12,9 +12,45 @@ namespace FrizerskiSalon.Forms
 {
     public partial class FrmIzvestajiPoRadniku : Form
     {
+        private IzvestajService _izvestajService = new IzvestajService();
+
         public FrmIzvestajiPoRadniku()
         {
             InitializeComponent();
+        }
+
+        private void FrmIzvestajiPoRadniku_Load(object sender, EventArgs e)
+        {
+            UcitajIzvestaj();
+        }
+
+        private void UcitajIzvestaj()
+        {
+            Dictionary<string, int> podaci = _izvestajService.BrojTerminaPoRadniku();
+
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Radnik");
+            dt.Columns.Add("Broj termina");
+
+            foreach (var stavka in podaci)
+                dt.Rows.Add(stavka.Key, stavka.Value);
+
+            dgvIzvestaj.DataSource = dt;
+
+    
+            chartIzvestaj.Series.Clear();
+            var serija = chartIzvestaj.Series.Add("Termini");
+            serija.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Bar;
+
+            foreach (var stavka in podaci)
+                serija.Points.AddXY(stavka.Key, stavka.Value);
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            _izvestajService.ExportUTxt();
+            MessageBox.Show("Izveštaj exportovan u Data\\izvestaj.txt", "Uspeh");
         }
     }
 }
