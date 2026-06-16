8dc695fe07bfa5b9394f0fea4bcb7f3e6e6cd12a
andjelamajstor <andjela2008.am@gmail.com>
2026-06-16 09:41:48 -0700

    dodati svi trmini i admin korisnici

--- FrizerskiSalon/Forms/FrmSviTermini.cs
+++ FrizerskiSalon/Forms/FrmSviTermini.cs
@@ -12,9 +12,62 @@ namespace FrizerskiSalon.Forms
 {
     public partial class FrmSviTermini : Form
     {
+        private TerminService _terminService = new TerminService();
+        private KorisnikService _korisnikService = new KorisnikService();
+
         public FrmSviTermini()
         {
             InitializeComponent();
+        }
+
+        private void FrmSviTermini_Load(object sender, EventArgs e)
+        {
+            List<Radnik> radnici = _korisnikService.SviRadnici();
+            cmbFilterRadnik.Items.Add("Svi");
+            foreach (Radnik r in radnici)
+                cmbFilterRadnik.Items.Add(r.KorisnickoIme);
+            cmbFilterRadnik.SelectedIndex = 0;
+
+            cmbFilterStatus.Items.Add("Svi");
+            cmbFilterStatus.Items.Add("ZAKAZAN");
+            cmbFilterStatus.Items.Add("OTKAZAN");
+            cmbFilterStatus.Items.Add("ODRADJEN");
+            cmbFilterStatus.SelectedIndex = 0;
+
+            dtpFilter.Value = DateTime.Now;
+
+            UcitajTermine();
+        }
+
+        private void UcitajTermine()
+        {
+            dgvTermini.DataSource = _terminService.SviTermini();
+        }
+
+        private void btnFilter_Click(object sender, EventArgs e)
+        {
+            List<Termin> termini = _terminService.SviTermini();
+
+            if (cmbFilterRadnik.SelectedItem.ToString() != "Svi")
+                termini = termini.Where(t => t.KorisnickoImeRadnika == cmbFilterRadnik.SelectedItem.ToString()).ToList();
+
+            if (cmbFilterStatus.SelectedItem.ToString() != "Svi")
+            {
+                StatusTermina status = (StatusTermina)Enum.Parse(typeof(StatusTermina), cmbFilterStatus.SelectedItem.ToString());
+                termini = termini.Where(t => t.Status == status).ToList();
+            }
+
+            termini = termini.Where(t => t.DatumVreme.Date == dtpFilter.Value.Date).ToList();
+
+            dgvTermini.DataSource = termini;
+        }
+
+        private void btnResetuj_Click(object sender, EventArgs e)
+        {
+            cmbFilterRadnik.SelectedIndex = 0;
+            cmbFilterStatus.SelectedIndex = 0;
+            dtpFilter.Value = DateTime.Now;
+            UcitajTermine();
         }
     }
 }
